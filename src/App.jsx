8511d6a574e2dd3468c19d623c21feb957c4e5f1
Panuwat Khannalae <panumataeh@gmail.com>
2022-10-28 21:39:37 +0700

    - crate route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import './App.css'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import HomePage from './page/HomePage'
+
+const queryClient = new QueryClient()
+
 function App() {
-  return <h1>App</h1>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    </QueryClientProvider>
+  )
 }
 
 export default App
