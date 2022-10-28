61d0274eac0aa926fed053b3f7170c373aba20c6
Panuwat Khannalae <panumataeh@gmail.com>
2022-10-28 23:29:47 +0700

    - create banner

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import useProductsAll from '../hooks/useProductsAll'
+// import useCategoriesAll  from '../hooks/useCategoriesAll'
+
+import Grid from '@mui/material/Unstable_Grid2'
+import Container from '@mui/material/Container'
 
 const HomePage = () => {
-  return <div>HomePage</div>
+  const { productsLoading, products, productsError } = useProductsAll()
+
+  console.log(products)
+
+  return (
+    <Container>
+      <Grid container>
+        <Grid xs={12}>
+          <img src="banner.webp" alt="banner" style={{ width: '100%' }} />
+        </Grid>
+      </Grid>
+    </Container>
+  )
 }
 
 export default HomePage
