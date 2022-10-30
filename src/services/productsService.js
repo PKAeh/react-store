5f324ec1f47cec3c72222f5e3d586e426044dbfe
Panuwat Khannalae <panumataeh@gmail.com>
2022-10-30 14:46:26 +0700

    - add api search and category

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -8,4 +8,16 @@ const getCategoriesAll = () => {
   )
 }
 
-export { getProductAll, getCategoriesAll }
+const getProductCategory = (skip = 0, limit = 20) => {
+  return fetch(
+    `https://dummyjson.com/products/category/smartphones?skip=${skip}&limit=${limit}`
+  ).then((res) => res.json())
+}
+
+const getProductSearch = (skip = 0, limit = 20, keyword) => {
+  return fetch(
+    `https://dummyjson.com/products/search?q=${keyword}&skip=${skip}&limit=${limit}`
+  ).then((res) => res.json())
+}
+
+export { getProductAll, getCategoriesAll, getProductCategory, getProductSearch }
