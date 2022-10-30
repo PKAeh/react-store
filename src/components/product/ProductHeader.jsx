ab77565c5459657028ad50d238a2fdc1c7f9475f
Panuwat Khannalae <panumataeh@gmail.com>
2022-10-30 23:02:55 +0700

    - remove padding

--- src/components/product/ProductHeader.jsx
+++ src/components/product/ProductHeader.jsx
@@ -4,11 +4,11 @@ import Typography from '@mui/material/Typography'
 
 const ProductHeader = ({ title, total }) => {
   return (
-    <Grid container columns={1}>
-      <Grid xs={1}>
+    <Grid container columns={1} sx={{ m: 0, pl: 0 }}>
+      <Grid xs={1} sx={{ pl: 0 }}>
         <Typography variant="h5">{title}</Typography>
       </Grid>
-      <Grid xs sx={{ pt: 0 }}>
+      <Grid xs sx={{ pt: 0, pl: 0 }}>
         <Typography sx={{ color: 'rgba(0,0,0,0.6)', pt: 0 }}>
           {total} ค้นพบสินค้าสำหรับ {`"${title}"`}
         </Typography>
