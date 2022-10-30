758067b936122d6699fa4f8ece229a6e407f3e56
Panuwat Khannalae <panumataeh@gmail.com>
2022-10-30 22:03:21 +0700

    -add onMouseEnter and onMouseLeave

--- src/components/product/ProductImage.jsx
+++ src/components/product/ProductImage.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Grid from '@mui/material/Unstable_Grid2'
 import ImageCover from './ImageCover'
 
 const ProductImage = ({ thumbnail, images }) => {
-  console.log(thumbnail)
+  const [thumbnailImage, setThumbnailImage] = useState(thumbnail)
+
+  const onMouseEnter = (path) => () => {
+    setThumbnailImage(path)
+  }
+
+  const onMouseLeave = () => {
+    setThumbnailImage(thumbnail)
+  }
 
   return (
     <Grid container>
       <Grid sx={{ width: '100%', p: 0 }}>
-        <ImageCover width={'100%'} height={'180px'} image={thumbnail} />
+        <ImageCover width={'100%'} height={'180px'} image={thumbnailImage} />
       </Grid>
       <Grid container>
         {images.map((path) => {
           return (
-            <Grid key={path}>
+            <Grid
+              key={path}
+              onMouseEnter={onMouseEnter(path)}
+              onMouseLeave={onMouseLeave}
+            >
               <ImageCover width={'30px'} height={'30px'} image={path} />
             </Grid>
           )
