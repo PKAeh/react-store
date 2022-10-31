f2f7f668f61272ee54e2ce74f3b5ff3e9dc5b8d3
Panuwat Khannalae <panumataeh@gmail.com>
2022-10-31 21:25:16 +0700

    - fix bug productcontent

--- src/components/product/ProductContent.jsx
+++ src/components/product/ProductContent.jsx
@@ -3,11 +3,9 @@ import Grid from '@mui/material/Unstable_Grid2'
 import ProductItem from '../product/ProductItem'
 
 const ProductContent = ({ products }) => {
-  const productList = products.slice(0, 6)
-
   return (
     <Grid container columns={4} sx={{ py: 0, px: 0 }}>
-      {productList.map((product) => {
+      {products.map((product) => {
         return (
           <Grid key={product.id} xs={1} sx={{ px: 0 }}>
             <ProductItem product={product} />
