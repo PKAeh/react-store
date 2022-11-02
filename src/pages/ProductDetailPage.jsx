fb0b267211291d7b64631eb6ff83cb647e9dd5f3
Panuwat Khannalae <panumataeh@gmail.com>
2022-11-02 21:31:43 +0700

    - call api product

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import useProductDetail from '../hooks/useProductDetail'
 
-const ProductDetail = () => {
-  return <div>ProductDetail</div>
+import { Container } from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2'
+import Loading from '../components/Loading'
+
+const ProductDetailPage = () => {
+  const { id } = useParams()
+  const { productDetailLoading, productDetail, productDetailError } =
+    useProductDetail(id)
+
+  if (productDetailLoading) {
+    return <Loading />
+  }
+
+  return (
+    <Container>
+      <Grid container>
+        <Grid></Grid>
+      </Grid>
+    </Container>
+  )
 }
 
-export default ProductDetail
+export default ProductDetailPage
