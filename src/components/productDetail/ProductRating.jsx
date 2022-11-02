4157d2ef00e5bf9ef40babc1f7957645bda9484c
Panuwat Khannalae <panumataeh@gmail.com>
2022-11-03 00:58:53 +0700

    - change text point

--- src/components/productDetail/ProductRating.jsx
+++ src/components/productDetail/ProductRating.jsx
@@ -29,7 +29,7 @@ const ProductRating = ({ rating }) => {
             textAlign: 'end',
           }}
         >
-          3133 คะแนน
+          {rating}
         </Typography>
       </Grid>
       <Grid
