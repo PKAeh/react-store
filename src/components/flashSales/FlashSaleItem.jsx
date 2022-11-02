6c3067956f045924cbf8d3df32df0a836bc39e81
Panuwat Khannalae <panumataeh@gmail.com>
2022-11-02 21:16:00 +0700

    - add link to productdetailpage

--- src/components/flashSales/FlashSaleItem.jsx
+++ src/components/flashSales/FlashSaleItem.jsx
@@ -4,12 +4,13 @@ import Grid from '@mui/material/Unstable_Grid2'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import ImageCover from '../product/ImageCover'
+import { Link } from 'react-router-dom'
 
 const FlashSaleItem = ({
   width,
   height,
   imageHeight,
-  product: { thumbnail, title, price, discountPercentage },
+  product: { thumbnail, title, price, discountPercentage, id },
 }) => {
   const priceText = price - (price * discountPercentage) / 100
 
@@ -24,43 +25,45 @@ const FlashSaleItem = ({
   }
 
   return (
-    <Paper
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      elevation={elevation}
-      sx={{ width: width, height: height, cursor: 'pointer' }}
-    >
-      <Grid container sx={{ width: '100%', m: 0, p: 0 }} columns={1}>
-        <Grid xs={1} sx={{ width: '100%', p: 0 }}>
-          <ImageCover width={'100%'} height={imageHeight} image={thumbnail} />
-        </Grid>
+    <Link to={`/product/${id}`} style={{ textDecoration: 'none' }}>
+      <Paper
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        elevation={elevation}
+        sx={{ width: width, height: height, cursor: 'pointer' }}
+      >
+        <Grid container sx={{ width: '100%', m: 0, p: 0 }} columns={1}>
+          <Grid xs={1} sx={{ width: '100%', p: 0 }}>
+            <ImageCover width={'100%'} height={imageHeight} image={thumbnail} />
+          </Grid>
 
-        <Grid xs={1}>
-          <Typography sx={{ fontWeight: 'bold' }}>{title}</Typography>
-        </Grid>
-        <Grid xs={1}>
-          <Typography sx={{ color: 'red' }}>{`฿${priceText.toFixed(
-            2
-          )}`}</Typography>
-        </Grid>
-        <Grid container>
-          <Grid sx={{ py: 0 }}>
-            <Typography
-              sx={{
-                textDecoration: 'line-through',
-                color: 'rgba(0,0,0,0.3)',
-                fontSize: '13px',
-              }}
-            >{`฿${price}`}</Typography>
+          <Grid xs={1}>
+            <Typography sx={{ fontWeight: 'bold' }}>{title}</Typography>
           </Grid>
-          <Grid sx={{ p: 0 }}>
-            <Typography
-              sx={{ fontSize: '13px' }}
-            >{` -${discountPercentage}%`}</Typography>
+          <Grid xs={1}>
+            <Typography sx={{ color: 'red' }}>{`฿${priceText.toFixed(
+              2
+            )}`}</Typography>
+          </Grid>
+          <Grid container>
+            <Grid sx={{ py: 0 }}>
+              <Typography
+                sx={{
+                  textDecoration: 'line-through',
+                  color: 'rgba(0,0,0,0.3)',
+                  fontSize: '13px',
+                }}
+              >{`฿${price}`}</Typography>
+            </Grid>
+            <Grid sx={{ p: 0 }}>
+              <Typography
+                sx={{ fontSize: '13px' }}
+              >{` -${discountPercentage}%`}</Typography>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </Link>
   )
 }
 
--- src/components/product/ProductItem.jsx
+++ src/components/product/ProductItem.jsx
@@ -3,9 +3,10 @@ import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Unstable_Grid2'
 import Typography from '@mui/material/Typography'
 import ProductImage from './ProductImage'
+import { Link } from 'react-router-dom'
 
 const ProductItem = ({
-  product: { thumbnail, title, price, discountPercentage, images },
+  product: { thumbnail, title, price, discountPercentage, images, id },
 }) => {
   const priceText = price - (price * discountPercentage) / 100
 
@@ -20,47 +21,49 @@ const ProductItem = ({
   }
 
   return (
-    <Paper
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      elevation={elevation}
-      sx={{ width: '98%', height: '380px', cursor: 'pointer' }}
-    >
-      <Grid container sx={{ width: '100%', m: 0, p: 0 }} columns={1}>
-        <Grid xs={1} sx={{ width: '100%', p: 0 }}>
-          <ProductImage thumbnail={thumbnail} images={images} />
-        </Grid>
-
-        <Grid xs={1}>
-          <Typography sx={{ fontWeight: 'bold', height: '2em' }}>
-            {title}
-          </Typography>
-        </Grid>
-
-        <Grid xs={1}>
-          <Typography sx={{ color: 'red' }}>{`฿${priceText.toFixed(
-            2
-          )}`}</Typography>
-        </Grid>
-
-        <Grid container>
-          <Grid sx={{ py: 0 }}>
-            <Typography
-              sx={{
-                textDecoration: 'line-through',
-                color: 'rgba(0,0,0,0.3)',
-                fontSize: '13px',
-              }}
-            >{`฿${price}`}</Typography>
+    <Link to={`/product/${id}`} style={{ textDecoration: 'none' }}>
+      <Paper
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        elevation={elevation}
+        sx={{ width: '98%', height: '380px', cursor: 'pointer' }}
+      >
+        <Grid container sx={{ width: '100%', m: 0, p: 0 }} columns={1}>
+          <Grid xs={1} sx={{ width: '100%', p: 0 }}>
+            <ProductImage thumbnail={thumbnail} images={images} />
           </Grid>
-          <Grid sx={{ p: 0 }}>
-            <Typography
-              sx={{ fontSize: '13px' }}
-            >{` -${discountPercentage}%`}</Typography>
+
+          <Grid xs={1}>
+            <Typography sx={{ fontWeight: 'bold', height: '2em' }}>
+              {title}
+            </Typography>
+          </Grid>
+
+          <Grid xs={1}>
+            <Typography sx={{ color: 'red' }}>{`฿${priceText.toFixed(
+              2
+            )}`}</Typography>
+          </Grid>
+
+          <Grid container>
+            <Grid sx={{ py: 0 }}>
+              <Typography
+                sx={{
+                  textDecoration: 'line-through',
+                  color: 'rgba(0,0,0,0.3)',
+                  fontSize: '13px',
+                }}
+              >{`฿${price}`}</Typography>
+            </Grid>
+            <Grid sx={{ p: 0 }}>
+              <Typography
+                sx={{ fontSize: '13px' }}
+              >{` -${discountPercentage}%`}</Typography>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </Link>
   )
 }
 
