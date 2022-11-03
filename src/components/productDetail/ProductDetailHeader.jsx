7613882d1d2ad48a093dbc44d6585ab75325a2e5
Panuwat Khannalae <panumataeh@gmail.com>
2022-11-03 23:03:39 +0700

    - add paper - change bg color

--- src/components/productDetail/ProductDetailHeader.jsx
+++ src/components/productDetail/ProductDetailHeader.jsx
@@ -4,20 +4,23 @@ import Grid from '@mui/material/Unstable_Grid2'
 import ProductDetailImage from './ProductDetailImage'
 import ProductDetailName from './ProductDetailName'
 import ProductDetailShipping from './ProductDetailShipping'
+import Paper from '@mui/material/Paper'
 
 const ProductDetailHeader = ({ product }) => {
   return (
-    <Grid container xs={12}>
-      <Grid xs>
-        <ProductDetailImage product={product} />
+    <Paper sx={{ p: 1, pb: 10 }}>
+      <Grid container xs={12}>
+        <Grid xs>
+          <ProductDetailImage product={product} />
+        </Grid>
+        <Grid xs={5} sx={{ ml: 2 }}>
+          <ProductDetailName product={product} />
+        </Grid>
+        <Grid xs>
+          <ProductDetailShipping />
+        </Grid>
       </Grid>
-      <Grid xs={5} sx={{ ml: 2 }}>
-        <ProductDetailName product={product} />
-      </Grid>
-      <Grid xs>
-        <ProductDetailShipping />
-      </Grid>
-    </Grid>
+    </Paper>
   )
 }
 
