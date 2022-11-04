7fbaa75179a6fd6bb9aeed74bdfbf7f0dad58713
Panuwat Khannalae <panumataeh@gmail.com>
2022-11-04 21:58:14 +0700

    - change description

--- src/components/productDetail/productDetailDescription/ProductDetailDescription.jsx
+++ src/components/productDetail/productDetailDescription/ProductDetailDescription.jsx
@@ -17,69 +17,7 @@ const ProductDetailDescription = ({ product: { title, description } }) => {
           sx={{ py: 3, borderBottom: '1px solid rgba(0,0,0,0.1)' }}
         >
           <Typography xs={1}>
-            {description} Lorem ipsum dolor sit amet consectetur, adipisicing
-            elit. Repellendus veritatis dicta in! Dolor architecto amet modi
-            pariatur optio facilis voluptas libero, molestiae eligendi cum
-            corrupti tenetur, animi dolore eveniet in. Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Corporis nemo quis tempore totam
-            non autem a voluptates inventore saepe sed iste, nihil porro fugit
-            et enim harum nostrum aliquid repellendus, doloremque, esse neque
-            ullam. Officiis aliquid veritatis expedita. Rerum provident
-            molestiae tenetur illum quod reiciendis esse cumque molestias minima
-            itaque! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Placeat animi eum tenetur, nobis ab ducimus quibusdam laboriosam
-            perspiciatis dolorem rerum blanditiis. Sed tempora exercitationem
-            corrupti, suscipit blanditiis eum explicabo quibusdam. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Perspiciatis, magnam
-            provident? Adipisci, quos! Necessitatibus sed error consequatur est
-            fugit dolorum mollitia perferendis, vitae, dolorem non eius illum
-            possimus hic saepe et repellat ipsam corporis nostrum pariatur
-            facilis! Quis, iste fuga libero quos velit rerum, vel quidem
-            officiis distinctio quia consectetur ullam. Laudantium perferendis
-            quisquam dignissimos placeat dicta quo facilis vel? Nobis repellat
-            alias quae asperiores quam dolorem voluptatem facilis laboriosam
-            voluptatum, placeat harum nostrum enim maiores consectetur vel quo.
-            Velit ipsam earum, laudantium laboriosam eum quia ex aperiam,
-            debitis nam quaerat sapiente vel cumque hic ullam delectus vero.
-            Tempora, nesciunt? Lorem, ipsum dolor sit amet consectetur
-            adipisicing elit. Maxime, consequuntur voluptas. Voluptate
-            consequuntur temporibus magni, possimus doloribus iste dignissimos
-            provident voluptatum, consequatur a earum, et suscipit sunt deleniti
-            reiciendis! In, sapiente debitis nisi assumenda quisquam quibusdam,
-            nihil incidunt, illum et animi dicta vitae! Culpa, doloribus quidem
-            adipisci qui fugiat quia, incidunt optio natus veritatis impedit
-            odio consectetur nisi molestiae ut aliquam. Dolor harum, quidem
-            quisquam totam rem nesciunt sequi voluptatum corrupti quae beatae.
-            Sed eum, obcaecati at dignissimos libero maiores quam nulla sequi
-            molestiae culpa beatae adipisci. Tenetur ab minus assumenda
-            blanditiis? Quam, magni? Cupiditate ipsum sit perspiciatis
-            consequatur aut sunt quos. Possimus quis odio optio perspiciatis
-            commodi ab nesciunt tenetur corporis, itaque, iure ducimus ex
-            eligendi id expedita placeat obcaecati reiciendis sed quia molestiae
-            eius nihil dolorum, eum tempora qui! Quam, nostrum, molestias
-            aliquid, dignissimos dicta provident dolorem enim corporis dolores
-            saepe voluptas cupiditate laboriosam voluptatem cum dolor
-            consectetur aspernatur fuga. Tenetur quod sequi sint amet error eum
-            voluptatem doloremque quisquam rerum. Cumque possimus in id
-            perspiciatis illo. Ullam beatae hic iusto quaerat. A ad neque
-            dolores fuga quod officia nulla enim. Minima sit animi cumque
-            asperiores ad aut a rerum explicabo. Quas eligendi voluptatibus
-            voluptates deserunt dolorum ducimus minima expedita. Eveniet
-            recusandae aperiam voluptates beatae necessitatibus tenetur cum,
-            quidem dignissimos sapiente, maiores est quos, quas harum nihil
-            commodi eius facere. Veniam explicabo numquam et optio ex inventore
-            repellat magni rem atque. Eaque, autem quis nisi illum, voluptatum
-            aut esse doloremque velit ipsum exercitationem asperiores at culpa
-            commodi perspiciatis. Fugit necessitatibus ex quod rem incidunt
-            dolore, ut eum non saepe earum! Tempore cupiditate voluptates
-            temporibus voluptas. Cumque nesciunt expedita molestiae magni cum
-            porro aperiam, rem necessitatibus impedit beatae totam nihil soluta,
-            eveniet dicta debitis omnis officia aliquam ut iusto animi. Cum
-            repudiandae minima aspernatur eum et, ipsum at voluptates excepturi
-            maxime eveniet. Ducimus fugiat maiores, praesentium sunt temporibus
-            ab dolores mollitia minima vero sed quod magnam omnis expedita.
-            Officia delectus aspernatur accusamus fuga sequi amet necessitatibus
-            dicta nesciunt rerum optio illum aliquid, nihil excepturi saepe
+            {description}
             atque culpa voluptatem odit ex voluptas quas consectetur officiis!
             Laborum, architecto. Corrupti fugiat labore sit quibusdam. Velit
             quae natus iste nesciunt ratione, enim similique totam inventore
